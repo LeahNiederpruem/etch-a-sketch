764d4d4bd45a6e8af09d40c381c628f418ecd0c6
Leon Niederpruem <leon.niederpruem@ivx.com>
2021-08-10 15:16:10 +0200

    Add a colorpicker

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const body = document.body;
 const wrapper = document.querySelector(".canvas");
 const clearBtn = document.querySelector(".clearBtn");
+const sliderControl = document.querySelector(".sliderControl");
+const sliderCounter = document.querySelector(".sliderCounter");
 
-let pixelSize = 10;
-
-const div = document.createElement("div");
+let pixelSize = sliderControl.value;
 
 function drawGrid(pixelSize) {
-  for (let i = 0; i < calculateBlocks(pixelSize); i++) {
+  for (let i = 0; i < blockAmount(pixelSize); i++) {
     let div = document.createElement("div");
 
     div.style.height = pixelSize + "px";
@@ -15,22 +15,44 @@ function drawGrid(pixelSize) {
 
     div.setAttribute("class", "canvasFill");
     div.addEventListener("mouseover", function () {
-      event.target.setAttribute("id", "colorBlack");
+      event.target.style.backgroundColor = getColor();
     });
 
     wrapper.appendChild(div);
   }
 }
 
-function calculateBlocks(pixelSize) {
+function clearGridItems() {
+  const canvasFillItems = document.querySelectorAll(".canvasFill");
+  canvasFillItems.forEach((item) => {
+    item.remove();
+  });
+}
+
+function updateCounter(value) {
+  sliderCounter.textContent = value;
+  clearGridItems();
+  drawGrid(value);
+}
+
+function blockAmount(pixelSize) {
   return (500 / pixelSize) * (500 / pixelSize);
 }
 
-clearBtn.addEventListener("click", function () {
+function clearCanvas() {
   const canvasFillItems = document.querySelectorAll(".canvasFill");
   canvasFillItems.forEach((item) => {
-    item.removeAttribute("id", "colorBlack");
+    item.style.backgroundColor = "white";
   });
+}
+
+function getColor() {
+    const colorPicker = document.querySelector(".colorPicker");
+    return colorPicker.value;
+}
+
+clearBtn.addEventListener("click", function () {
+  clearCanvas();
 });
 
 drawGrid(pixelSize);
