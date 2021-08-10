7068d8df75eb857879966de47c79483b3455ea6e
Leon Niederpruem <leon.niederpruem@ivx.com>
2021-08-10 10:05:58 +0200

    Add basic styling

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const body = document.body;
-const wrapper = document.querySelector(".canvasWrapper");
+const wrapper = document.querySelector(".canvas");
 
 const div = document.createElement("div");
 div.setAttribute("class", "canvasFill");
@@ -14,7 +14,7 @@ wrapper.appendChild(div);
 //     }
 // }
 
-for (let i = 1; i < 625; i++) {
+for (let i = 1; i < 2500; i++) {
   let div = document.createElement("div");
   div.setAttribute("class", "canvasFill");
   wrapper.appendChild(div);
