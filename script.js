25acd1415fecbef3c8334cea4ddd88d9af8e13dd
Leon Niederpruem <leon.niederpruem@ivx.com>
2021-08-10 11:43:01 +0200

    Add basic function

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 const body = document.body;
 const wrapper = document.querySelector(".canvas");
+const clearBtn = document.querySelector(".clearBtn");
 
 const div = document.createElement("div");
 div.setAttribute("class", "canvasFill");
 
 wrapper.appendChild(div);
 
-// for (let i = 0; i < 5; i++) {
-//     for (let j = 0; j < 5; j++) {
-//         let div = document.createElement('div')
-//         div.setAttribute('class', 'canvasFill')
-//         wrapper.appendChild(div)
-//     }
-// }
-
-for (let i = 1; i < 2500; i++) {
+for (let i = 0; i < 625; i++) {
   let div = document.createElement("div");
   div.setAttribute("class", "canvasFill");
+  div.addEventListener("mouseover", function () {
+    event.target.setAttribute("id", "colorBlack");
+  });
   wrapper.appendChild(div);
 }
+
+clearBtn.addEventListener("click", function () {
+  const canvasFillItems = document.querySelectorAll(".canvasFill");
+  canvasFillItems.forEach((item) => {
+    item.removeAttribute("id", "colorBlack");
+  });
+});
